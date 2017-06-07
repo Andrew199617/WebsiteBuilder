870c0d83470f1823993b3a5fe8f36dbf32fe9872
Andrew199617 <andrewmauro199617@yahoo.com>
2017-06-07 01:13:45 -0600

    Push

--- src/neumont/csc280/WebsiteBuilder/controller/CreatePage.java
+++ src/neumont/csc280/WebsiteBuilder/controller/CreatePage.java
@@ -27,14 +27,60 @@ public class CreatePage extends HttpServlet {
 	public static Page page = null;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String templateNum = request.getParameter("templateNum");
+
+		Session dbSession = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction transaction = dbSession.beginTransaction();
+		if(templateNum != null)
+		{
+			try {
+				
+				page.setTemplateNum(templateNum);
+				
+				dbSession.update(page);
+				
+				transaction.commit();
+			}
+			catch(Exception ex) {
+				transaction.rollback();
+				ex.printStackTrace();
+			}
+		}
+		File welcomePage = null;
+		switch(templateNum) {
+		case "1":
+			welcomePage = new File(DoLogin.MyPath + "/firstTemplate.html");
+			break;
+		case "2":
+			welcomePage = new File(DoLogin.MyPath + "/secondTemplate.html");
+			break;
+		case "3":
+			welcomePage = new File(DoLogin.MyPath + "/thirdTemplate.html");
+			break;
+		}
+		
+		BufferedReader br = new BufferedReader(new FileReader(welcomePage));
+	    StringBuilder sb = new StringBuilder();
+		try {
+		    String line = br.readLine();
+		
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append("\n");
+		        line = br.readLine();
+		    }
+		} finally {
+		    br.close();
+		}
+		
+		response.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html");
+		response.getWriter().write(sb.toString());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String pageName = request.getParameter("websiteName");
-		
-		String templateNum = request.getParameter("templateNum");
-		
 		
 		
 		HttpSession session = request.getSession(true);
@@ -58,24 +104,7 @@ public class CreatePage extends HttpServlet {
 				ex.printStackTrace();
 			}
 		
-		} else if(templateNum != null)
-		{
-			try {
-				
-				page.setName(pageName);
-				page.setOwner(user);
-				
-				dbSession.save(page);
-				
-				transaction.commit();
-			}
-			catch(Exception ex) {
-				transaction.rollback();
-				ex.printStackTrace();
-			}
-		}
-		
-		
+		} 
 		
 		File welcomePage = new File(DoLogin.MyPath + "/WelcomePage.html");
 		
